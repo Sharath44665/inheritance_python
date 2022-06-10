ebb5752aaea286290f0953ffd0ad1270a331f190
Sharath <sharathwelcomesyouin@gmail.com>
2022-06-10 22:27:35 +0530

    add inheritance

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 class Animal:
     def __init__(self):
-        self.eyes=2
+        self.eyes = 2
+
     def breath(self):
         print("inhale, exhale")
 
-class Fish:
-    
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def breath(self):
+        super().breath()
+        print("breath under water")
+
     def swim(self):
         print("moving in water")
 
-nemo= Fish()
+
+nemo = Fish()
 nemo.swim()
+nemo.breath()
