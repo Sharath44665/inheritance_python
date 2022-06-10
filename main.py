769352505521553b4a842a701cb45434bb5b50de
Sharath <sharathwelcomesyouin@gmail.com>
2022-06-10 22:23:10 +0530

    add fish class and call swim

---
+++ main.py
@@ -0,0 +1,13 @@
+class Animal:
+    def __init__(self):
+        self.eyes=2
+    def breath(self):
+        print("inhale, exhale")
+
+class Fish:
+    
+    def swim(self):
+        print("moving in water")
+
+nemo= Fish()
+nemo.swim()
